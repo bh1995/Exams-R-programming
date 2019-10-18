fa989314ce17dce9eb83d31e4c8f404e77b52209
Agustín Valencia <valencia.ag@gmail.com>
2019-10-18 22:41:00 +0200

    replace objects with sailors

--- 20181023/Batory.R
+++ 20181023/Batory.R
@@ -1,58 +1,69 @@
 Batory <- setRefClass("Batory",
-                      fields = list(
-                        fuel="numeric", 
-                        max_cargo_weight="numeric",
-                        cargo_weight="numeric",
-                        objects="list"),
-                      
-                      methods = list(
-                        initialize = function(fuel, max_cargo_weight)
-                        {
-                          "Fuel and cargo weight"
-                          #validate
-                          fuel <<- fuel
-                          max_cargo_weight <<- max_cargo_weight
-                          cargo_weight <<- fuel
-                          objects <<- list(Name="fuel", Weight = fuel)
-                        },
-                        
-                        equip_Batory = function(name, weight)
-                        {
-                          "quepasa"
-                          #validate
-                          objects$Name <<- append(objects$Name, name)
-                          objects$Weight <<- append(objects$Weight, weight)
-                          cargo_weight <<- cargo_weight + weight
-                          stopifnot(cargo_weight<=max_cargo_weight)
-                        },
-                        
-                        embark_sailor = function(s_weight)
-                        {
-                          total_weight <- cargo_weight + s_weight
-                          if (total_weight <= max_cargo_weight)
-                          {
-                            equip_Batory("sailor", s_weight)
-                          }
-                          else
-                          {
-                            for (i in length(objects$Name)) {
-                              if (objects$Name[i] != "fuel" || objects$Name[i] != "sailor")
-                              {
-                                1+1
-                              }
-                            }
-                          }
-                        },
-                        
-                        print = function()
-                        {
-                          cat("\n")
-                          cat("In the vessel of the batory you can find:")
-                          cat("\n")
-                          
-                        }
-                        
-                      )
+    fields = list(
+      fuel="numeric", 
+      max_cargo_weight="numeric",
+      cargo_weight="numeric",
+      objects="list"),
+    
+    methods = list(
+      initialize = function(fuel, max_cargo_weight) {
+        "Fuel and cargo weight"
+        #validate
+        fuel <<- fuel
+        max_cargo_weight <<- max_cargo_weight
+        cargo_weight <<- fuel
+        objects <<- list(Name="fuel", Weight = fuel)
+      },
+      
+      equip_Batory = function(name, weight) {
+        "quepasa"
+        #validate
+        if ( cargo_weight + weight <= max_cargo_weight ) {
+          objects$Name <<- append(objects$Name, name)
+          objects$Weight <<- append(objects$Weight, weight)
+          update_weight(weight)
+        } else {
+          stop("Exceeded max cargo weight")
+        }
+      },
+      
+      update_weight = function(new_weight) {
+        cargo_weight <<- cargo_weight + new_weight
+      },
+      
+      embark_sailor = function(s_weight) {
+        total_weight <- cargo_weight + s_weight
+        if (total_weight <= max_cargo_weight) {
+          equip_Batory("sailor", s_weight)
+        } else {
+          remove_items(s_weight)
+          equip_Batory("sailor", s_weight)
+        }
+      },
+      
+      remove_items = function(weight) {
+        for (i in 1:length(objects$Name)) {
+          if ( objects$Name[i] != "fuel" &
+               objects$Name[i] != "sailor" ) {
+            update_weight(-objects$Weight[i])
+            objects$Name <<- objects$Name[-i]
+            objects$Weight <<- objects$Weight[-i]
+          }
+          if (cargo_weight + weight <= max_cargo_weight) {
+            break
+          }
+        }
+        
+      },
+      
+      print = function() {
+        cat("\n")
+        cat("In the vessel of the batory you can find:")
+        cat("\n")
+        
+      }
+      
+    )
                       
 )
 
